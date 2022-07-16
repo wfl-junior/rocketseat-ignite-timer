cb750fed7a8a1c40039290e028f6918d28228b52
Wallace Júnior <wflj1997@hotmail.com>
2022-07-16 15:54:57 -0300

    Página: History

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,1 +1,71 @@
-export const History: React.FC = () => <h1>History</h1>;
+import { HistoryContainer, HistoryList } from "./styles";
+
+export const History: React.FC = () => (
+  <HistoryContainer>
+    <h1>Meu histórico</h1>
+
+    <HistoryList>
+      <table>
+        <thead>
+          <tr>
+            <th>Tarefa</th>
+            <th>Duração</th>
+            <th>Início</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Tarefa</td>
+            <td>20 minutos</td>
+            <td>Há cerca de 2 meses</td>
+            <td>Concluído</td>
+          </tr>
+
+          <tr>
+            <td>Tarefa</td>
+            <td>20 minutos</td>
+            <td>Há cerca de 2 meses</td>
+            <td>Concluído</td>
+          </tr>
+
+          <tr>
+            <td>Tarefa</td>
+            <td>20 minutos</td>
+            <td>Há cerca de 2 meses</td>
+            <td>Concluído</td>
+          </tr>
+
+          <tr>
+            <td>Tarefa</td>
+            <td>20 minutos</td>
+            <td>Há cerca de 2 meses</td>
+            <td>Concluído</td>
+          </tr>
+
+          <tr>
+            <td>Tarefa</td>
+            <td>20 minutos</td>
+            <td>Há cerca de 2 meses</td>
+            <td>Concluído</td>
+          </tr>
+
+          <tr>
+            <td>Tarefa</td>
+            <td>20 minutos</td>
+            <td>Há cerca de 2 meses</td>
+            <td>Concluído</td>
+          </tr>
+
+          <tr>
+            <td>Tarefa</td>
+            <td>20 minutos</td>
+            <td>Há cerca de 2 meses</td>
+            <td>Concluído</td>
+          </tr>
+        </tbody>
+      </table>
+    </HistoryList>
+  </HistoryContainer>
+);
