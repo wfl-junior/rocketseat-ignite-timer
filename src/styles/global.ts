dbc0957fd916c97398e975c76b7f877e46a42216
Wallace Júnior <wflj1997@hotmail.com>
2022-07-16 15:25:33 -0300

    Página: Home

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -15,11 +15,16 @@ export const GlobalStyle = createGlobalStyle`
   body {
     background-color: ${({ theme }) => theme["gray-900"]};
     color: ${({ theme }) => theme["gray-300"]};
+    -webkit-font-smoothing: antialised;
   }
 
   body, input, textarea, button {
     font-family: "Roboto", sans-serif;
     font-weight: 400;
     font-size: 1rem;
+  }
+
+  a, button {
+    cursor: pointer;
   }
 `;
