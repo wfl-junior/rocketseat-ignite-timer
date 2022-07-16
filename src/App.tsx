0830e25cc27462aa979dac265970cf24d0bb2512
Wallace Júnior <wflj1997@hotmail.com>
2022-07-16 13:26:26 -0300

    Estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { ThemeProvider } from "styled-components";
 import { Button } from "./components/Button";
+import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 
 export const App: React.FC = () => (
   <ThemeProvider theme={defaultTheme}>
+    <GlobalStyle />
+
     <Button variant="primary" />
     <Button variant="secondary" />
     <Button variant="success" />
