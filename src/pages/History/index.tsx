75b30351036ddbe65302c4b4d2798bee86da4c9c
Wallace Júnior <wflj1997@hotmail.com>
2022-07-17 00:45:15 -0300

    Formatação de data

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,5 @@
+import { formatDistanceToNow } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
 import { useCyclesContext } from "../../contexts/CyclesContext";
 import { HistoryContainer, HistoryList, Status } from "./styles";
 
@@ -24,7 +26,13 @@ export const History: React.FC = () => {
               <tr key={cycle.id}>
                 <td>{cycle.task}</td>
                 <td>{cycle.minutesAmount} minutos</td>
-                <td>{cycle.startDate.toLocaleString()}</td>
+
+                <td>
+                  {formatDistanceToNow(cycle.startDate, {
+                    addSuffix: true,
+                    locale: ptBR,
+                  })}
+                </td>
 
                 <td>
                   {cycle.finishedDate && (
